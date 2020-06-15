832a0471def6a15f3abb452f224ab03344edeee7
Elton Tozatto <eltontozatto@gmail.com>
2020-06-15 10:10:32 -0300

    Editar vendedor e exceções de serviço personalizadas

---
+++ SalesWebMvc/Services/Exceptions/NotFoundExcepetion.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundExcepetion : ApplicationException
+    {
+        public NotFoundExcepetion(string message) : base(message) { }
+    }
+}
